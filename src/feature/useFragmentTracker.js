a2adaab660206448c2e2cd18752fc0d2c8d63c81
Kevin <kersv@Kevins-MBP.fios-router.home>
2024-07-06 07:00:26 -0400

    save state to localstorage

--- src/feature/useFragmentTracker.js
+++ src/feature/useFragmentTracker.js
@@ -1,26 +1,54 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { ORIGIN_FRAGMENT_COST, HEXA_MASTERY_FRAGMENT_COST, ENHANCEMENT_FRAGMENT_COST, COMMON_CORE_COST } from "../constants/fragmentCost";
 
 const useFragmentTracker = () => {
+    const getInitialState = (key, initialValue) => {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : initialValue;
+    };
+
     // Current Level
-    const [valueOri, setOri] = useState(1);
-    const [valueHexa, setHexa] = useState(0);
-    const [valueSecondHexa, setSecondHexa] = useState(0);
-    const [valueEnhace1, setEnhace1] = useState(0);
-    const [valueEnhace2, setEnhace2] = useState(0);
-    const [valueEnhace3, setEnhace3] = useState(0);
-    const [valueEnhace4, setEnhace4] = useState(0);
-    const [valueCommon, setCommon] = useState(0);
+    const [valueOri, setOri] = useState(getInitialState('valueOri', 1));
+    const [valueHexa, setHexa] = useState(getInitialState('valueHexa', 0));
+    const [valueSecondHexa, setSecondHexa] = useState(getInitialState('valueSecondHexa', 0));
+    const [valueEnhace1, setEnhace1] = useState(getInitialState('valueEnhace1', 0));
+    const [valueEnhace2, setEnhace2] = useState(getInitialState('valueEnhace2', 0));
+    const [valueEnhace3, setEnhace3] = useState(getInitialState('valueEnhace3', 0));
+    const [valueEnhace4, setEnhace4] = useState(getInitialState('valueEnhace4', 0));
+    const [valueCommon, setCommon] = useState(getInitialState('valueCommon', 0));
+
 
     // Desire Level
-    const [valueDesireOri, setDesireOri] = useState(30);
-    const [valueDesireHexa, setDesireHexa] = useState(30);
-    const [valueDesireSecondHexa, setDesireSecondHexa] = useState(30);
-    const [valueDesireEnhace1, setDesireEnhace1] = useState(30);
-    const [valueDesireEnhace2, setDesireEnhace2] = useState(30);
-    const [valueDesireEnhace3, setDesireEnhace3] = useState(30);
-    const [valueDesireEnhace4, setDesireEnhace4] = useState(30);
-    const [valueDesireCommon, setDesireCommon] = useState(30);
+    const INITIAL_DESIRE_VALUE = 30;
+    const [valueDesireOri, setDesireOri] = useState(getInitialState('valueDesireOri', INITIAL_DESIRE_VALUE));
+    const [valueDesireHexa, setDesireHexa] = useState(getInitialState('valueDesireHexa', INITIAL_DESIRE_VALUE));
+    const [valueDesireSecondHexa, setDesireSecondHexa] = useState(getInitialState('valueDesireSecondHexa', INITIAL_DESIRE_VALUE));
+    const [valueDesireEnhace1, setDesireEnhace1] = useState(getInitialState('valueDesireEnhace1', INITIAL_DESIRE_VALUE));
+    const [valueDesireEnhace2, setDesireEnhace2] = useState(getInitialState('valueDesireEnhace2', INITIAL_DESIRE_VALUE));
+    const [valueDesireEnhace3, setDesireEnhace3] = useState(getInitialState('valueDesireEnhace3', INITIAL_DESIRE_VALUE));
+    const [valueDesireEnhace4, setDesireEnhace4] = useState(getInitialState('valueDesireEnhace4', INITIAL_DESIRE_VALUE));
+    const [valueDesireCommon, setDesireCommon] = useState(getInitialState('valueDesireCommon', INITIAL_DESIRE_VALUE));
+
+    useEffect(() => {
+        localStorage.setItem('valueOri', JSON.stringify(valueOri));
+        localStorage.setItem('valueHexa', JSON.stringify(valueHexa));
+        localStorage.setItem('valueSecondHexa', JSON.stringify(valueSecondHexa));
+        localStorage.setItem('valueEnhace1', JSON.stringify(valueEnhace1));
+        localStorage.setItem('valueEnhace2', JSON.stringify(valueEnhace2));
+        localStorage.setItem('valueEnhace3', JSON.stringify(valueEnhace3));
+        localStorage.setItem('valueEnhace4', JSON.stringify(valueEnhace4));
+        localStorage.setItem('valueCommon', JSON.stringify(valueCommon));
+        localStorage.setItem('valueDesireOri', JSON.stringify(valueDesireOri));
+        localStorage.setItem('valueDesireHexa', JSON.stringify(valueDesireHexa));
+        localStorage.setItem('valueDesireSecondHexa', JSON.stringify(valueDesireSecondHexa));
+        localStorage.setItem('valueDesireEnhace1', JSON.stringify(valueDesireEnhace1));
+        localStorage.setItem('valueDesireEnhace2', JSON.stringify(valueDesireEnhace2));
+        localStorage.setItem('valueDesireEnhace3', JSON.stringify(valueDesireEnhace3));
+        localStorage.setItem('valueDesireEnhace4', JSON.stringify(valueDesireEnhace4));
+        localStorage.setItem('valueDesireCommon', JSON.stringify(valueDesireCommon));
+    }, [valueOri, valueHexa, valueSecondHexa, valueEnhace1, valueEnhace2, valueEnhace3, valueEnhace4, valueCommon,
+        valueDesireOri, valueDesireHexa, valueDesireSecondHexa, valueDesireEnhace1, valueDesireEnhace2, valueDesireEnhace3, valueDesireEnhace4, valueDesireCommon]);
+
 
     const handleChange = (e, setter) => {
         const newValue = Number(e.target.value);
