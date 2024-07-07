005df839a64b29d794153f903fb1af33b0d639e2
Kevin <kersv@Kevins-MBP.fios-router.home>
2024-07-07 08:58:43 -0400

    add 2sec loader

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,38 @@
-import '../App.css'
-import Tracker from '../feature/Tracker'
-import DesireSkill from '../feature/DesireSkill'
-import useFragmentTracker from '../feature/useFragmentTracker'
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import Tracker from '../feature/Tracker';
+import DesireSkill from '../feature/DesireSkill';
+import useFragmentTracker from '../feature/useFragmentTracker';
+import loader from '../assets/loader.gif';
+
 const Home = () => {
     const hook = useFragmentTracker();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div>
-            <h3>Maplestory 6th Job Tracker</h3>
-            <div style={{display:'flex'}}>
-                <Tracker props={hook}/>
-                <DesireSkill props={hook}/>
-            </div>
+            {loading ? (
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+                    <img src={loader} alt="Loading..."/>
+                </div>
+            ) : (
+                <div>
+                    <h3>Maplestory 6th Job Tracker</h3>
+                    <div style={{ display: 'flex' }}>
+                        <Tracker props={hook} />
+                        <DesireSkill props={hook} />
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default Home;
